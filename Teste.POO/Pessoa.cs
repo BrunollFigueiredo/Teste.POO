75bb2e49dff08c9837297828df56903d92dddf38
025.029779 <025.029779@C305-3045441>
2026-02-03 16:04:00 -0300

    Adicionar arquivos de projeto.

---
+++ Teste.POO/Pessoa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Teste.POO
+{
+     class Pessoa
+    {
+     
+        public string Nome;
+        public int Idade;
+        public string Cpf;
+        public string Data;
+        
+        public Pessoa(string nome, int idade,string cpf, string data)
+        {
+            Nome = nome;
+            Idade = idade;
+            Cpf = cpf;
+            Data = data;
+        }
+        
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}, CPF {Cpf}, Data de Nascimento {Data}");
+        }
+        public string ObterIdade()
+        {
+            return Idade >= 18 ? "Maior de idade" : "Menor de idade";
+        }
+    }
+}
---
+++ Teste.POO/Program.cs
@@ -0,0 +1,11 @@
+﻿using Teste.POO;
+
+
+    Pessoa obj1 = new Pessoa("Maria", 25,"0952345-11","24/08/2004");
+    obj1.ExibirInformacoes();
+Pessoa obj2 = new Pessoa("Ricardo", 27, "0952345-11", "24/08/2004");
+obj2.ExibirInformacoes();
+string retorno = obj2.ObterIdade();
+Console.WriteLine($"Ricardo é {retorno}");
+
+
